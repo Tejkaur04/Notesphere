a254f6304f0eb976a514aa2607b72fad39ff1559
Priyam Aggarwal <146617286+Tejkaur04@users.noreply.github.com>
2025-03-08 02:03:11 +0530

    updated index.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Home from './Pages/Home';
+import ImageComponent from './Components/ImageComponent';
+
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ImageComponent />} />
+        <Route path="/login" element={<Login setLoggedIn={setLoggedIn} />} />
+        <Route path="/signup" element={<Signup setLoggedIn={setLoggedIn} />} />
+        <Route path="/home" element={<Home loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+      </Routes>
+    </Router>
   );
 }
 
